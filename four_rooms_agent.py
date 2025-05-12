30771a9a84982b752aca49bd075d11ef15db9e8e
shaun <shaunleib35@gmail.com>
2025-05-12 15:25:23 -0700

    random walk

--- four_rooms_agent.py
+++ four_rooms_agent.py
@@ -13,6 +13,12 @@ from q_learning_lambda import QLearningLambda
 #     "agent": 10,
 # }
 
+# DIR_TO_IDX  = {
+#     "right" : 0,
+#     "down" : 1,
+#     "left" : 2,
+#     "up" : 3
+# }
 
 """
 obs['image'].type = np.ndarray
@@ -20,26 +26,38 @@ obs['image'].shape = (19, 19, 3)
 (X, Y, OBJECT_IDX)
 """
 
+def episode(env, max_steps):
+    for i in range(max_steps):
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, _ = env.step(action)
+        env.render()
+        if terminated or truncated:
+            obs, _ = env.reset()
 
+def trail():
+    pass
 
 
 
 def main() -> None:
 
     # Make the environment
-    env = gym.make("MiniGrid-FourRooms-v0")
+    env = gym.make("MiniGrid-FourRooms-v0", render_mode = "human") # add  render_mode = "human" for visual
 
-    env_obs = SymbolicObsWrapper(env)
-    obs,_ = env_obs.reset()
+    env = SymbolicObsWrapper(env)
+    obs, _ = env.reset()
+    print(type(env))
+    print(env)
 
-    print(obs["direction"])
+    print(f"{obs['direction'] =}")
     print(obs['mission'])
     print(f"{obs['image'].shape = } ")
     agent = np.where(obs['image'][:, :, 2] == 10)
     goal = np.where(obs['image'][:, :, 2] == 8)
     print(f"{agent =}")
     print(f"{goal = }")
-    q_learning_lambda = QLearningLambda(obs['image'], agent, goal)
+    episode(env, 100)
+        
 
 
 
